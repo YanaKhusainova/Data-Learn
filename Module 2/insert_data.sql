ed7b2346b638311efd3c5793cae0522cf6ea995d
YanaKhusainova <63101910+YanaKhusainova@users.noreply.github.com>
2022-01-25 14:51:59 +0300

    Update insert_data.sql

--- Module 2/insert_data.sql
+++ Module 2/insert_data.sql
@@ -1,75 +1,94 @@
---insert values product_dim
-insert into public.product_dim
-select 100+row_number() over() as prod_id,
-				category,
-				subcategory,
-				product_name,
-				product_id
-			from (select distinct category, 
-                                    	      subcategory, 
-                                   	      product_name,
-                                    	      product_id  from public.orders) as a;
-                                    
---insert values geografy_dim
-insert into public.product_dim
-select 100+row_number() over() as prod_id,
-				category,
-				subcategory,
-				product_name,
-				product_id
-			from (select distinct category, 
-                                    	      subcategory,
-                                    	      product_name,
-                                    	      product_id   from public.orders) as a;
-                                    
---insert order_date
-insert into public.order_date_dim
-select 100+row_number() over() as order_date_id,
-				order_date,
-				extract(year from orders.order_date),
-				extract(quarter from orders.order_date),
-				extract(month from orders.order_date),
-				extract(week from orders.order_date),
-				extract(day from orders.order_date)
-			from orders
-				
---insert shipping_dim
-insert into public.shipping_dim
-select 100+row_number() over() as ship_id,
-				ship_mode
-			from orders
-							
---insert ship_date
-insert into public.ship_date_dim
-select 100+roe_number() over() as ship_date_dim_as,
-				ship_date,
-				extract(year from orders.shup_date),
-				extract(quarter from orders.shup_date),
-				extract(month from orders.shup_date),
-				extract(week from orders.shup_date)
-			from orders
-			
---insert sales_fact
-insert into sales_fact
-select 	row_number() over() as row_id,		
-						order_id,
-						order_date_id,
-						ship_date_dim_id,
-						ship_id,
-						geo_id,
-						product_dim_id,
-						sales,
-						quantity,
-						discount,
-						profit
-			from orders as o
-inner join 
-	order_date_dim as od on o.order_date = od.order_date
-inner join 
-	ship_date_dim as sh on o.ship_date = sh.ships_date
-inner join 
-	shipping_dim as ss on o.ship_mode = ss.ship_mode
-inner join 
-	geography_dim as gp on o.city = gp.city and o.state=gp.state and o.postal_code = gp.postal_code
-inner join 
-	product_dim as pr on o.product_id = pr.product_id
+--SHIPPING
+
+--deleting rows
+truncate table dw.shipping_dim;
+
+--generating ship_id and inserting ship_mode from orders
+insert into dw.shipping_dim 
+select 100+row_number() over(), ship_mode from (select distinct ship_mode from stg.orders ) a;
+--checking
+select * from dw.shipping_dim sd; 
+
+
+--CUSTOMER
+
+--deleting rows
+truncate table dw.customer_dim;
+--inserting
+insert into dw.customer_dim 
+select 100+row_number() over(), customer_id, customer_name from (select distinct customer_id, customer_name from stg.orders ) a;
+--checking
+select * from dw.customer_dim cd;  
+
+
+--GEOGRAPHY
+
+--deleting rows
+truncate table dw.geo_dim;
+--generating geo_id and inserting rows from orders
+insert into dw.geo_dim 
+select 100+row_number() over(), country, city, state, postal_code from (select distinct country, city, state, postal_code from stg.orders ) a;
+--data quality check
+select distinct country, city, state, postal_code from dw.geo_dim
+where country is null or city is null or postal_code is null;
+
+-- City Burlington, Vermont doesn't have postal code
+update dw.geo_dim
+set postal_code = '05401'
+where city = 'Burlington'  and postal_code is null;
+
+--also update source file
+update stg.orders
+set postal_code = '05401'
+where city = 'Burlington'  and postal_code is null;
+
+
+select * from dw.geo_dim
+where city = 'Burlington'
+
+
+--PRODUCT
+
+--deleting rows
+truncate table dw.product_dim ;
+--
+insert into dw.product_dim 
+select 100+row_number() over () as prod_id ,product_id, product_name, category, subcategory, segment from (select distinct product_id, product_name, category, subcategory, segment from stg.orders ) a;
+--checking
+select * from dw.product_dim cd; 
+
+
+--METRICS
+
+insert into dw.sales_fact 
+select
+	 100+row_number() over() as sales_id
+	 ,cust_id
+	 ,order_date as  order_date_id
+	 ,ship_date as  ship_date_id
+	 ,p.prod_id
+	 ,s.ship_id
+	 ,geo_id
+	 ,o.order_id
+	 ,sales
+	 ,profit
+     ,quantity
+	 ,discount
+from stg.orders o 
+inner join dw.shipping_dim s on o.ship_mode = s.shipping_mode
+inner join dw.geo_dim g on o.postal_code = g.postal_code and g.country=o.country and g.city = o.city and o.state = g.state --City Burlington doesn't have postal code
+inner join dw.product_dim p on o.product_name = p.product_name and o.segment=p.segment and o.subcategory=p.sub_category and o.category=p.category and o.product_id=p.product_id 
+inner join dw.customer_dim cd on cd.customer_id=o.customer_id and cd.customer_name=o.customer_name 
+
+
+--do you get 9994rows?
+select count(*) from dw.sales_fact sf
+inner join dw.shipping_dim s on sf.ship_id=s.ship_id
+inner join dw.geo_dim g on sf.geo_id=g.geo_id
+inner join dw.product_dim p on sf.prod_id=p.prod_id
+inner join dw.customer_dim cd on sf.cust_id=cd.cust_id;
+
+
+
+
+
